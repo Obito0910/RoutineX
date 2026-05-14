9235d6a34a9528fb42c94cbf5971f3fd1c16e951
Muhammad Imad Khan <imadyusafzai349@gmail.com>
2026-05-14 10:44:20 +0500

    Refactor and improve task manager UI

--- source_code/RouteinX.cpp
+++ source_code/RouteinX.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <fstream>
-#include <cstring>
-#include <limits>
+#include <string>
+#include <limits> 
 using namespace std;
 
 const int max_task = 100;
@@ -11,14 +11,15 @@ struct Task{
     bool completed;
     char time[6];
 };
+
 // function declarations
 void initaltask(Task* tasks, int& taskCount);
 void Menu();
 void addtask(Task* tasks, int& taskCount);
 void displaytasks(Task* tasks, int taskCount);
 void marktaskcompleted(Task* tasks, int taskCount);
-void savefile(Task* tasks, int taskCount, const char* filename);
-void loadfromfile(Task* tasks, int& taskCount, const char* filename);
+void savefile(Task* tasks, int taskCount, const char *filename);
+void loadfromfile(Task* tasks, int& taskCount, const char *filename);
 void clearinputbuffer();
 
 int main() {
@@ -26,7 +27,17 @@ int main() {
     Task tasks[max_task];
     int taskCount = 0;
     const char* filename = "tasks.txt";
-    cout<<"DAILY ROUTINE MANAGER"<<endl;
+    //cout<<"Daily Routine manager"<<endl;                                                                                 
+                                                                                    
+cout<<" ▄▄▄▄▄▄                                     ██                         ▄▄▄  ▄▄▄ "<<endl;
+cout<<" ██▀▀▀▀██                        ██         ▀▀                          ██▄▄██  "<<endl;
+cout<<" ██    ██   ▄████▄   ██    ██  ███████    ████     ██▄████▄   ▄████▄     ████   "<<endl;
+cout<<" ███████   ██▀  ▀██  ██    ██    ██         ██     ██▀   ██  ██▄▄▄▄██     ██    "<<endl;
+cout<<" ██  ▀██▄  ██    ██  ██    ██    ██         ██     ██    ██  ██▀▀▀▀▀▀    ████   "<<endl;
+cout<<" ██    ██  ▀██▄▄██▀  ██▄▄▄███    ██▄▄▄   ▄▄▄██▄▄▄  ██    ██  ▀██▄▄▄▄█   ██  ██  "<<endl;
+cout<<" ▀▀    ▀▀▀   ▀▀▀▀     ▀▀▀▀ ▀▀     ▀▀▀▀   ▀▀▀▀▀▀▀▀  ▀▀    ▀▀    ▀▀▀▀▀   ▀▀▀  ▀▀▀ "<<endl;
+                                                                            
+                                                                                                                                                                                                                     
     loadfromfile(tasks, taskCount, filename);
     cout<<"Loaded "<<taskCount<<" tasks from previous session."<<endl;
     int choice;
@@ -36,7 +47,7 @@ int main() {
         cout<<"Enter your choice (1-6): ";
         cin>>choice;
         clearinputbuffer();
-        switch(choice) {
+        switch(choice){
             case 1:
                 addtask(tasks, taskCount);
                 break;
@@ -57,126 +68,111 @@ int main() {
             case 6:
                 savefile(tasks, taskCount, filename);
                 cout<<"Tasks saved."<<endl;
-                cout<<"Thank you for using Daily Routine Manager!"<<endl;
+                cout<<"Thank you for using RoutineX!"<<endl;
+                cout<<"Visit https://obito0910.github.io/RoutineX  for GUI version"<<endl;
                 break;
             default:
                 cout<<"Wrong Choice!"<<endl; 
-                cout<<"Please enter a number between 1-6."<<endl;
-        }
+                cout<<"Please enter a number between (1-6)."<<endl;
+            }
 
-    } while(choice != 6);
+    }while(choice != 6);
     return 0;
 }
-
 // initialize task list
-void initaltask(Task* tasks, int& taskCount) {
+void initaltask(Task *tasks, int& taskCount) {
     taskCount = 0;
 }
-
 // Display menu
 void Menu() {
-    cout<<"   MAIN MENU "<<endl;
-    cout<<"1. Add a New Task"<<endl;
-    cout<<"2. Display All Tasks"<<endl;
-    cout<<"3. Mark Task as Completed"<<endl;
-    cout<<"4. Save Tasks to File"<<endl;
-    cout<<"5. Reload Tasks from File"<<endl;
-    cout<<"6. Exit Program"<<endl;
-    cout<<"__________________________"<<endl;
+    cout<<"<====Main Menu====>"<<endl;
+    cout<<"1. Add a new task"<<endl;
+    cout<<"2. Display all tasks"<<endl;
+    cout<<"3. Mark task as completed"<<endl;
+    cout<<"4. Save tasks to file"<<endl;
+    cout<<"5. Reload tasks from file"<<endl;
+    cout<<"6. Exit program"<<endl;
+    cout<<"######################"<<endl;
 }
-
 // add task
 void addtask(Task*tasks, int& taskCount){
     if(taskCount >= max_task) {
-        cout << "Task list is full! Cannot add more tasks.\n\n";
+        cout<<"Task list is full! Cannot add more tasks."<<endl;;
         return;
     }
-    Task* currentTask = &tasks[taskCount];
-    cout<<"\n@@@ ADD NEW TASK @@@\n";
+    Task *currentTask = &tasks[taskCount];
+    cout<<"===== ADD NEW TASK =====" <<endl;
     cout<<"Enter task description (max 100 chars): ";
-    cin.getline(currentTask->description, 101);
+    cin.getline(tasks[taskCount].description , 101);
     cout<<"Enter time for task (HH:MM format): ";
-    cin.getline(currentTask->time, 6);
+    cin.getline(tasks[taskCount].time, 6);
     do {
         cout<<"Enter priority "<<endl;
         cout<<"1-High"<<endl;
         cout<<"2-Medium"<<endl;
         cout<<"3-Low"<<endl;
-        cin>>currentTask->priority;
+        cin>>tasks[taskCount].priority;
         clearinputbuffer();
-        if(currentTask->priority < 1 || currentTask->priority>3) {
-            cout<<"Invalid priority!"<<endl;
+        if (tasks[taskCount].priority < 1 || tasks[taskCount].priority > 3){
+            cout<<"Invalid priority"<<endl;
             cout<<"Please enter 1, 2, or 3."<<endl;
         }
-    } while(currentTask->priority<1 || currentTask->priority>3);
-    currentTask->completed = false;
+    } 
+    while (tasks[taskCount].priority < 1 || tasks[taskCount].priority > 3);
+    tasks[taskCount].completed = false;
     taskCount++;
-    cout<<"Task added successfully!"<<endl;
+    cout<<"Task added successfully"<<endl;
 }
-
 // display tasks
-void displaytasks(Task*tasks, int taskCount) {
-    if(taskCount == 0) {
-        cout<<"No tasks to display."<<endl<<endl;
+void displaytasks(Task tasks[], int taskCount){
+    if (taskCount == 0){
+        cout<<"No tasks to display."<<endl;
         return;
     }
-    Task* ptr=tasks;
-    for(int i = 0; i < taskCount; i++) {
-        cout<<"Task #"<<(i + 1)<<endl;
-        cout<<"Description: "<<ptr->description<<endl;
-        cout<<"Time: "<<ptr->time<<endl;
-        cout<<"Priority: "<<endl;
-        if(ptr->priority == 1){ 
-            cout<<"High";
+    for(int i = 0; i < taskCount; i++){
+        cout<<"Task #" << i + 1<<endl;
+        cout<<"Description: "<<tasks[i].description<<endl;
+        cout<<"Time: "<<tasks[i].time<<endl;
+        cout<<"Priority: ";
+        if (tasks[i].priority == 1){
+            cout << "High" << endl;
+        }else if (tasks[i].priority == 2){
+            cout<<"Medium"<<endl;
+        }else{
+            cout<<"Low"<<endl;
         }
-        else if(ptr->priority == 2){
-             cout<<"Medium";
+        cout<<"Status: ";
+        if (tasks[i].completed == true){
+            cout<<"Completed"<<endl;
+        }else{
+            cout<<"Pending"<<endl;
         }
-        else{
-             cout<<"Low";
-        }
-        cout<<"\nStatus: ";
-        if(ptr->completed){
-            cout<<"COMPLETED"<<endl;
-        }
-        else {
-            cout<<"PENDING"<<endl;
-        }
-        cout<<"_______________________________"<<endl;
-        ptr++;
+        cout<<"_____________________________"<<endl;
     }
-    cout<<endl;
 }
-
 // mark task completed
-void marktaskcompleted(Task* tasks, int taskCount) {
+void marktaskcompleted(Task* tasks, int taskCount){
+    int num;
+
     if(taskCount == 0){
-        cout<<"No tasks available."<<endl<<endl;
+        cout<<"No tasks available."<<endl;
         return;
     }
     displaytasks(tasks, taskCount);
-    int num;
-    cout<<"Enter task number to mark completed: ";
+    cout<<"Enter task number: ";
     cin>>num;
-    clearinputbuffer();
-    if(num<1 || num>taskCount) {
-        cout<<"Invalid task number!"<<endl;
+    if(num<1 || num>taskCount){
+        cout<<"Invalid task number"<<endl;
         return;
     }
-    Task* ptr=tasks+(num-1);
-    if(ptr->completed) {
-        cout<<"Task already completed!"<<endl;
-    } else {
-        ptr->completed = true;
-        cout<<"Task marked as completed!"<<endl;
-    }
+    tasks[num - 1].completed = true;
+    cout<<"Task marked as completed"<<endl;
 }
-
 // save to file
-void savefile(Task* tasks, int taskCount, const char* filename) {
+void savefile(Task* tasks, int taskCount, const char* filename){
     ofstream file(filename);
     if(!file) {
-        cout << "Error opening file!"<<endl;
+        cout << "Error opening file"<<endl;
         return;
     }
     file<<taskCount<<endl;
@@ -188,7 +184,6 @@ void savefile(Task* tasks, int taskCount, const char* filename) {
     }
     file.close();
 }
-
 // load from file
 void loadfromfile(Task* tasks, int& taskCount, const char* filename) {
     ifstream file(filename);
@@ -207,9 +202,9 @@ void loadfromfile(Task* tasks, int& taskCount, const char* filename) {
     }
     file.close();
 }
-
 // clear input buffer
 void clearinputbuffer() {
     cin.clear();
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
 }
+
